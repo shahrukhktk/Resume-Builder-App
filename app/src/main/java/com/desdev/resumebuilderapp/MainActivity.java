1159f49dac6b267211901d9710e0c29b2bd2bc27
shahrukhktk <shahrukhktk760@gmail.com>
2019-09-11 21:26:07 +0500

    Second Commit

--- app/src/main/java/com/desdev/resumebuilderapp/MainActivity.java
+++ app/src/main/java/com/desdev/resumebuilderapp/MainActivity.java
@@ -2,13 +2,31 @@ package com.desdev.resumebuilderapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void CreateResume(View view)
+    {
+        startActivity(new Intent(getApplicationContext(), CreateResumes_Activity.class));
+    }
+
+    public void ResumesCatalog(View view)
+    {
+
+    }
+
+    public void YourResumes(View view)
+    {
+
     }
 }
