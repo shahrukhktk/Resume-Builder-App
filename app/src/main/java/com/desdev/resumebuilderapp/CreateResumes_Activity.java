65fc414064123fb783d92f143cc40073e3cb295e
shahrukhktk <shahrukhktk760@gmail.com>
2019-09-12 12:43:01 +0500

    Third Commit

--- app/src/main/java/com/desdev/resumebuilderapp/CreateResumes_Activity.java
+++ app/src/main/java/com/desdev/resumebuilderapp/CreateResumes_Activity.java
@@ -1,8 +1,16 @@
 package com.desdev.resumebuilderapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.desdev.resumebuilderapp.Fragments.CareerObjective_Fragment;
+import com.desdev.resumebuilderapp.Fragments.PersonalInfo_fragment;
 
 public class CreateResumes_Activity extends AppCompatActivity {
 
@@ -10,5 +18,90 @@ public class CreateResumes_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_resumes_);
+    }
+
+    public void CreateCV(View view)
+    {
+        if(view == findViewById(R.id.personal_info))
+        {
+            Fragment fragment = new PersonalInfo_fragment();
+            FragmentManager fm = getSupportFragmentManager();
+            FragmentTransaction ft = fm.beginTransaction();
+            ft.replace(R.id.frame_container, fragment);
+            ft.commit();
+        }
+
+        if(view == findViewById(R.id.objective))
+        {
+            Fragment fragment = new CareerObjective_Fragment();
+            FragmentManager fm = getSupportFragmentManager();
+            FragmentTransaction ft = fm.beginTransaction();
+            ft.replace(R.id.frame_container, fragment);
+            ft.commit();
+        }
+
+        if(view == findViewById(R.id.education))
+        {
+            Fragment fragment = new PersonalInfo_fragment();
+            FragmentManager fm = getSupportFragmentManager();
+            FragmentTransaction ft = fm.beginTransaction();
+            ft.replace(R.id.frame_container, fragment);
+            ft.commit();
+        }
+
+        if(view == findViewById(R.id.skills))
+        {
+            Fragment fragment = new PersonalInfo_fragment();
+            FragmentManager fm = getSupportFragmentManager();
+            FragmentTransaction ft = fm.beginTransaction();
+            ft.replace(R.id.frame_container, fragment);
+            ft.commit();
+        }
+
+        if(view == findViewById(R.id.experience))
+        {
+            Fragment fragment = new PersonalInfo_fragment();
+            FragmentManager fm = getSupportFragmentManager();
+            FragmentTransaction ft = fm.beginTransaction();
+            ft.replace(R.id.frame_container, fragment);
+            ft.commit();
+        }
+
+        if(view == findViewById(R.id.languages))
+        {
+            Fragment fragment = new PersonalInfo_fragment();
+            FragmentManager fm = getSupportFragmentManager();
+            FragmentTransaction ft = fm.beginTransaction();
+            ft.replace(R.id.frame_container, fragment);
+            ft.commit();
+        }
+
+        if(view == findViewById(R.id.projects))
+        {
+            Fragment fragment = new PersonalInfo_fragment();
+            FragmentManager fm = getSupportFragmentManager();
+            FragmentTransaction ft = fm.beginTransaction();
+            ft.replace(R.id.frame_container, fragment);
+            ft.commit();
+        }
+
+        if(view == findViewById(R.id.achievements))
+        {
+            Fragment fragment = new PersonalInfo_fragment();
+            FragmentManager fm = getSupportFragmentManager();
+            FragmentTransaction ft = fm.beginTransaction();
+            ft.replace(R.id.frame_container, fragment);
+            ft.commit();
+        }
+
+        if(view == findViewById(R.id.references))
+        {
+            Fragment fragment = new PersonalInfo_fragment();
+            FragmentManager fm = getSupportFragmentManager();
+            FragmentTransaction ft = fm.beginTransaction();
+            ft.replace(R.id.frame_container, fragment);
+            ft.commit();
+        }
+
     }
 }
--- app/src/main/java/com/desdev/resumebuilderapp/Fragments/PersonalInfo_fragment.java
+++ app/src/main/java/com/desdev/resumebuilderapp/Fragments/PersonalInfo_fragment.java
@@ -1,4 +1,4 @@
-package com.desdev.resumebuilderapp;
+package com.desdev.resumebuilderapp.Fragments;
 
 import android.content.Context;
 import android.net.Uri;
@@ -10,13 +10,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.desdev.resumebuilderapp.R;
+
 public class PersonalInfo_fragment extends Fragment
 {
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_personal_info_fragment, container, false);
+        View view = inflater.inflate(R.layout.fragment_personal_info_fragment, container, false);
+        return view;
     }
 
 }
